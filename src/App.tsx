b1ed4fd660c300db4b1e5fa455c18673a0f85cb9
Probir-Ray <somironroy.need@gmail.com>
2024-05-15 19:50:41 +0600

    Install Chakra UI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <h2>Hello World</h2>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
+}
+
+export default App;
